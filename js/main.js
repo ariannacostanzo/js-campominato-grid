c506033ae7ac54294dde03e37be8b41823ae1ce4
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-10 16:37:58 +0100

    fixed last issues and added comments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 // Todo Raccolgo gli elementi
 // Todo Creo un event listener
 // Todo Creo le celle secondo le dimensioni della griglia
+// Todo Cambio la dimensione delle celle in base al select
 
 const gridElement = document.getElementById('grid');
 const form = document.querySelector('form');
 const gridSizeOptionElement = document.querySelector('select')
 
 form.addEventListener('submit', (e) => {
+    // *impedisco il reload al submit e svuoto la griglia 
     e.preventDefault();
     gridElement.innerHTML = '';
 
+    // *Ottengo le dimensioni di rows e cols in base al select
     const gridSizeOption = gridSizeOptionElement.value; 
     logSomething(gridSizeOption);
     let rows = 10;
@@ -26,13 +29,16 @@ form.addEventListener('submit', (e) => {
         gridSizeCode = 'small'
     }
     
-    
     const cellQuantity = rows * cols;
 
+    //* creo tante celle quante rows * cols
     for (let i = 1; i <= cellQuantity; i++) {
         const cell = createCell(i);
+
+        //* aggiungo la classe css per regolare la dimensione delle celle
         cell.classList.add(gridSizeCode);
 
+        //* gestisco il cambio colore al click delle celle
         cell.addEventListener('click', () => {
             logSomething(i);
             cell.classList.toggle('clicked');
@@ -41,6 +47,4 @@ form.addEventListener('submit', (e) => {
         gridElement.appendChild(cell);
     }
     
-
-
 });
